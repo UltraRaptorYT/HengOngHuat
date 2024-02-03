be2747e5a5f9f3fb7c8732a7fa32b71758c88fd5
Soh Hong Yu <sohhongyu@gmail.com>
2024-02-04 04:12:04 +0800

    Added Ending Page and Camera Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactNode, useRef } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import CustomButton from "./components/CustomButton";
+import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/components/ui/use-toast";
+import Webcam from "react-webcam";
 import { cn } from "./lib/utils";
+
+type bodyClassType = "title_page" | "end_page" | "flowers" | "gold" | "light";
 
 function App() {
   const { toast } = useToast();
-  let [name, setName] = useState<string>("");
-  let [bodyClass, setBodyClass] = useState<string>("title_page");
+  let docRef = useRef(null);
+  let [name, setName] = useState<string>("a");
+  let [bodyClass, setBodyClass] = useState<bodyClassType>("title_page");
   let [language, setLanguage] = useState<"EN" | "中文">("EN");
+  let [width, setWidth] = useState<number>();
+  let [height, setHeight] = useState<number>();
 
-  let LANGUAGE_MAP = {
+  const videoConstraints = {
+    width: width,
+    height: height,
+    facingMode: "environment",
+  };
+
+  type LANGUAGE_TYPE = {
+    header: ReactNode;
+    input: string;
+    flower: ReactNode;
+    gold: ReactNode;
+    light: ReactNode;
+    warning: string;
+    congrats: string;
+  };
+
+  type LANGUAGE_MAP_TYPE = {
+    EN: LANGUAGE_TYPE;
+    中文: LANGUAGE_TYPE;
+  };
+
+  let LANGUAGE_MAP: LANGUAGE_MAP_TYPE = {
     EN: {
       header: <span>Choose Your Offering</span>,
       input: "Enter Your Name",
       flower: <span>Flowers</span>,
       gold: <span>Gold Ingots</span>,
       light: <span>Candles</span>,
-      warning: <span>Please enter name before choosing offering</span>,
+      warning: "Please enter name before choosing offering",
+      congrats: "CONGRATULATIONS",
     },
     中文: {
       header: <div className="text-[2.6rem] font-bold mt-8">选择您的贡品</div>,
@@ -27,27 +56,55 @@ function App() {
       flower: <span>鲜花</span>,
       gold: <span>金宝</span>,
       light: <span>蜡烛</span>,
-      warning: <span>Please enter name before choosing offering</span>,
+      warning: "请输入您的名字才选择您的贡品",
+      congrats: "恭喜",
     },
   };
 
-  function changeState(state: string) {
+  function changeState(state: bodyClassType) {
     if (name.trim().length == 0) {
       return toast({
-        title: "Please enter name before choosing offering",
+        title: LANGUAGE_MAP[language]["warning"],
         variant: "destructive",
-        duration: 1000,
+        duration: 2500,
       });
     }
     setBodyClass(state);
     console.log(name);
   }
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (bodyClass == "flowers" || bodyClass == "gold" || bodyClass == "light") {
+      setTimeout(() => {
+        setBodyClass("end_page");
+      }, 10000);
+    }
+  }, [bodyClass]);
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: true })
+      .then(function (stream) {
+        const videoTrack = stream.getVideoTracks()[0];
+        if (videoTrack) {
+          videoTrack.stop();
+        }
+      })
+      .catch(function (error) {
+        console.error("Error accessing video stream:", error);
+      });
+    if (docRef && docRef.current) {
+      setWidth((docRef.current as HTMLDivElement).clientWidth);
+      setHeight((docRef.current as HTMLDivElement).clientHeight);
+    }
+  }, []);
 
   return (
-    <div className={cn(bodyClass, "relative", language)}>
-      <div className="absolute top-2 right-2">
+    <div
+      className={cn(bodyClass, "relative min-h-[100dvh] h-full", language)}
+      ref={docRef}
+    >
+      <div className="absolute top-2 right-2 z-10">
         <Button
           variant={"outline"}
           onClick={() =>
@@ -58,47 +115,94 @@ function App() {
           {language == "EN" ? <span className="font-bold">中文</span> : "EN"}
         </Button>
       </div>
-      <div className="flex flex-col min-h-[100dvh] h-full items-center gap-5 mx-auto w-[250px] pt-20">
-        <h1 className="text-4xl textOutline textShadow text-[#5e2222] text-center">
-          {LANGUAGE_MAP[language]["header"]}
-        </h1>
-        <Input
-          onInput={(e) => {
-            setName((e.target as HTMLInputElement).value);
-          }}
-          type="text"
-          id="name"
-          placeholder={LANGUAGE_MAP[language]["input"]}
-          className={cn(
-            "text-center my-6 rounded-none border-4 border-white text-white placeholder:text-white bg-[#374f66] flex h-full w-full px-1 py-3 text-lg focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-white focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            language == "中文" ? "text-2xl" : ""
-          )}
-        />
-        <CustomButton
-          className={cn(language == "中文" ? "font-semibold" : "", "text-2xl")}
-          clickHandler={() => {
-            changeState("flowers");
-          }}
-        >
-          {LANGUAGE_MAP[language]["flower"]}
-        </CustomButton>
-        <CustomButton
-          className={cn(language == "中文" ? "font-semibold" : "", "text-2xl")}
-          clickHandler={() => {
-            changeState("gold");
-          }}
-        >
-          {LANGUAGE_MAP[language]["gold"]}
-        </CustomButton>
-        <CustomButton
-          className={cn(language == "中文" ? "font-semibold" : "", "text-2xl")}
-          clickHandler={() => {
-            changeState("light");
-          }}
-        >
-          {LANGUAGE_MAP[language]["light"]}
-        </CustomButton>
-      </div>
+      {bodyClass == "title_page" ? (
+        <div className="flex flex-col min-h-[100dvh] h-full items-center gap-5 mx-auto w-[250px] pt-20">
+          <h1 className="text-4xl textOutline textShadow text-[#5e2222] text-center">
+            {LANGUAGE_MAP[language]["header"]}
+          </h1>
+          <Input
+            onInput={(e) => {
+              setName((e.target as HTMLInputElement).value);
+            }}
+            type="text"
+            id="name"
+            placeholder={LANGUAGE_MAP[language]["input"]}
+            className={cn(
+              "text-center my-6 rounded-none border-4 border-white text-white placeholder:text-white bg-[#374f66] flex h-full w-full px-1 py-3 text-lg focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-white focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+              language == "中文" ? "text-2xl" : ""
+            )}
+          />
+          <CustomButton
+            className={cn(
+              language == "中文" ? "font-semibold" : "",
+              "text-2xl"
+            )}
+            clickHandler={() => {
+              changeState("flowers");
+            }}
+          >
+            {LANGUAGE_MAP[language]["flower"]}
+          </CustomButton>
+          <CustomButton
+            className={cn(
+              language == "中文" ? "font-semibold" : "",
+              "text-2xl"
+            )}
+            clickHandler={() => {
+              changeState("gold");
+            }}
+          >
+            {LANGUAGE_MAP[language]["gold"]}
+          </CustomButton>
+          <CustomButton
+            className={cn(
+              language == "中文" ? "font-semibold" : "",
+              "text-2xl"
+            )}
+            clickHandler={() => {
+              changeState("light");
+            }}
+          >
+            {LANGUAGE_MAP[language]["light"]}
+          </CustomButton>
+        </div>
+      ) : bodyClass == "end_page" ? (
+        <div className="flex flex-col min-h-[100dvh] h-full justify-center items-center gap-24 mx-auto w-[250px] text-[#e3e04b] relative">
+          <div
+            className={cn(
+              "text-2xl text-center endPageText absolute left-0 right-0 top-[22.5%]",
+              language == "中文" ? "font-semibold text-[2.6rem]" : ""
+            )}
+          >
+            {LANGUAGE_MAP[language]["congrats"] + " " + name}
+          </div>
+          <div className="text-8xl font-bold glowText 中文">大吉</div>
+        </div>
+      ) : (
+        <div>
+          <Webcam
+            audio={false}
+            width={width}
+            height={height}
+            videoConstraints={videoConstraints}
+          ></Webcam>
+          <div
+            className={cn(
+              "absolute left-0 right-0 z-10",
+              bodyClass == "flowers"
+                ? "bottom-[-10%]"
+                : bodyClass == "light"
+                ? "bottom-[-15%] translate-x-[10px]"
+                : "bottom-[-20%]"
+            )}
+          >
+            <img src={`/img/${bodyClass}.png`} />
+          </div>
+        </div>
+      )}
+      <Toaster
+        className={cn(language == "中文" ? "text-2xl font-normal" : "")}
+      />
     </div>
   );
 }
--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -5,19 +5,20 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
-import { useToast } from "@/components/ui/use-toast"
+} from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
+import { cn } from "@/lib/utils";
 
-export function Toaster() {
-  const { toasts } = useToast()
+export function Toaster({ className }: { className?: string }) {
+  const { toasts } = useToast();
 
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
+            <div className={cn("grid gap-1")}>
+              {title && <ToastTitle className={className}>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
               )}
@@ -25,9 +26,9 @@ export function Toaster() {
             {action}
             <ToastClose />
           </Toast>
-        )
+        );
       })}
       <ToastViewport />
     </ToastProvider>
-  )
+  );
 }
